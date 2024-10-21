4e950b807bc71f4a2fe3b6d307b8f5898b45e453
mr.Robot <zanid227@yandex.ru>
2024-10-21 18:53:19 +0300

    Разработка бота-библиотекаря

---
+++ app.py
@@ -0,0 +1,34 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+BOT_TOKEN = os.getenv('TOKEN')
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Ся!\nНапиши мне что-нибудь')
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твоё сообщение'
+    )
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
