2e7713952f6cd84d0bbce266424b40e48fc9a827
mr.Robot <zanid227@yandex.ru>
2024-10-21 18:59:13 +0300

    Реализовал второй метод регистрации команд в диспетчере

--- app.py
+++ app.py
@@ -12,12 +12,10 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-@dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Ся!\nНапиши мне что-нибудь')
 
 
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -29,6 +27,10 @@ async def process_help_command(message: Message):
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+
+dp.message.register(process_start_command, Command(commands=['start']))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
