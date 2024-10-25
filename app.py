afac1b3a8a6259fa46202beec9935e2a337a73bc
mr.Robot <zanid227@yandex.ru>
2024-10-25 18:33:10 +0300

    Закончил бота-библиотекаря

--- app.py
+++ app.py
@@ -1,185 +1,42 @@
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+import asyncio
+import logging
 
-from dotenv import load_dotenv
-from environs import Env
-from pprint import pprint
-from random import randint
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
-
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env('TOKEN')
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-ATTEMPTS = 5
-
-users = {}
+from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
+from keyboards.main_menu import set_main_menu
 
 
-def check_user_in_base(message: Message):
-    if message.from_user.id not in users:
-        users[message.from_user.id] = {
-            'in_game': False,
-            'secret_number': None,
-            'attempts': None,
-            'total_games': 0,
-            'wins': 0
-        }
+logger = logging.getLogger(__name__)
 
 
-def generate_num_from_100() -> int:
-    return randint(0, 100)
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s'
+               )
+    
+    logger.info('Starting bot')
 
-
-@dp.message(~F.text.startswith('привет'))
-async def process_hello(message: Message):
-    await message.answer(text='И тебе привет!')
-
-
-async def process_start_command(message: Message):
-    check_user_in_base(message)
-    await message.answer(
-        'Привет!\nДавайте сыграем в игру "Угадай число"?\n\n'
-        'Чтобы получить правила игры и список доступных '
-        'команд - отправьте команду /help'
-        )
-
-
-async def process_help_command(message: Message):
-    await message.answer(
-        f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
-        f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
-        f'попыток\n\nДоступные команды:\n/help - правила '
-        f'игры и список команд\n/cancel - выйти из игры\n'
-        f'/stat - посмотреть статистику\n\nДавай сыграем?'
+    config: Config = load_config()
+    
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
+    dp = Dispatcher()
+
+    await set_main_menu(bot)
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
-async def process_stat_command(message: Message):
-    user = users[message.from_user.id]
-    await message.answer(
-        f'Всего иго сыграно: {user["total_games"]}\n'
-        f'Игр выиграно: {user["wins"]}'
-    )
-
-
-async def process_cancel_command(message: Message):
-    user = users[message.from_user.id]
-    if user['in_game']:
-        user['in_game'] = False
-        await message.answer(
-            'Вы вышши из игры. Если захотите сыграть '
-            'снова - пишите об этом'
-        )
-    else:
-        await message.answer(
-            'А мы и так с вами не играем. '
-            'Может, сыграем разок?'
-        )
-
-
-async def process_positive_answer(message: Message):
-    user = users[message.from_user.id]
-    if not user['in_game']:
-        user['secret_number'] = generate_num_from_100()
-        user['attempts'] = ATTEMPTS
-        user['in_game'] = True
-        await message.answer(
-            'Ура!\nЯ загадал число от 1 до 100, '
-            'попробуй угадать!'
-        )
-    else:
-        await message.answer(
-            'Пока мы играем в игру я могу '
-            'реагировать только на числа от 1 до 100 '
-            'и команды /cancel и /stat'
-        )
-
-
-async def process_negative_answer(message: Message):
-    if not users[message.from_user.id]['in_game']:
-        await message.answer(
-            'Жаль :(\n\nЕсли захотите поиграть - просто '
-            'напишите об этом'
-        )
-    else:
-        await message.answer(
-            'Мы же сейчас с вами играем. Присылайте, '
-            'пожалуйста, числа от 1 до 100'
-        )
-
-
-async def process_numbers_answer(message: Message):
-    user = users[message.from_user.id]
-    user_num = int(message.text)
-    if user['in_game']:
-        if user_num == user['secret_number']:
-            user['in_game'] = False
-            user['total_games'] += 1
-            user['wins'] += 1
-            await message.answer(
-                'УРА! Вы угадали число!\n\n'
-                'Может, сыграем ещё?'
-            )
-        elif user_num > user['secret_number']:
-            user['attempts'] -= 1
-            await message.reply(
-                'Моё число меньше'
-            )
-        elif user_num < user['secret_number']:
-            user['attempts'] -= 1
-            await message.reply(
-                'Моё число больше'
-            )
-        
-        if not user['attempts']:
-            user['in_game'] = False
-            user['total_games'] += 1
-            await message.answer(
-                f'К сожалению, у вас больше не осталось '
-                f'попыток. Вы проиграли :(\n\nМое число '
-                f'было {user["secret_number"]}\n\nДавайте '
-                f'сыграем ещё?'
-            )
-    else:
-        await message.answer(
-            'Мы ещё не играем. Хотите сыграть?'
-        )
-
-
-async def process_other_answer(message: Message):
-    if users[message.from_user.id]['in_game']:
-        await message.answer(
-            'Мы же сейчас с вами играем. '
-            'Присылайте числа от 1 до 100'
-        )
-    else:
-        await message.answer(
-            'Я довольно ограниченный бот, давайте '
-            'просто сыграем в игру?'
-        )
-
-
-dp.message.register(process_start_command, CommandStart())
-dp.message.register(process_help_command, Command(commands=['help']))
-dp.message.register(process_stat_command, Command(commands=['stat']))
-dp.message.register(process_cancel_command, Command(commands=['cancel']))
-dp.message.register(process_positive_answer, F.text.lower().in_(
-    ['да', 'давай', 'сграем', 'игра', 'играть', 'хочу играть'])
-    )
-dp.message.register(process_negative_answer, F.text.lower().in_(
-    ['нет', 'не', 'не хочу', 'не буду'])
-    )
-dp.message.register(process_numbers_answer, 
-                    lambda x: x.text and x.text.isdigit()
-                    and 1 <= int(x.text) <= 100)
-dp.message.register(process_other_answer)
-
-
-if __name__ == '__main__':
-    dp.run_polling(bot)
+asyncio.run(main())
